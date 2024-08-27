7bcd61285fe03e591e9575dc211933ad809238b6
JM <mjoensen15@gmail.com>
2024-08-27 21:56:16 +0800

    first commit

---
+++ src/Header.jsx
@@ -0,0 +1,19 @@
+import myLogo from './assets/logo.png'
+
+function Header(){
+    return(
+        <header>
+            <div className="logo">
+                <img src={myLogo} alt="logo yan dapat dito kasi"></img>
+            </div>
+            <div className="navlinks">
+                <a href="about" className="nav-btn">ABOUT ME</a>
+                <a href="project" className="nav-btn">PROJECTS</a>
+                <a href="contact" className="nav-btn">CONTACT ME</a>
+            </div>
+        </header>
+
+    );
+}
+
+export default Header
---
+++ src/Hero.jsx
@@ -0,0 +1,30 @@
+
+function Hero(){
+    return(
+        <section>
+            <div className="container">
+                <div className="hero">
+                <div className="mini-heading">
+                    <p>Hi there, my name is</p>
+                <div className="name">
+                    <h4>JM JUNGCO</h4>
+                <div className="content">
+                    <h4>I design and develop for the users.</h4>
+                <div className="mini-intro">
+                    <p>I’m a student at Western Institute of Technology partaking the course of Information of Technology.</p>
+                    <div className="up-line"></div>
+                <div className="scroll-line">
+                    <p className="line"> scroll</p>
+                </div>
+                <div className="bot-line"></div>
+                </div>
+                </div>
+                </div>
+                </div>
+                </div>
+                </div>
+        </section>
+    );
+}
+
+export default Hero
