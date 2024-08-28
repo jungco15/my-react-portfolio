9f6c402b0fdc6f7ff02352407aad58ffa2853bf3
john micheal jungco <jmjungcobsit1@gmail.com>
2024-08-28 22:04:24 +0800

    commit

---
+++ src/About.jsx
@@ -0,0 +1,29 @@
+
+
+function About(){
+    return(
+
+        <div className="container">
+            <div className="section-header">
+                <h5></h5>
+            </div>
+            <div className="line"></div>
+            <div className="content">
+                 <div className="body-left">
+                    <p></p>
+                 </div>
+            <div className="body-right">
+                    <img></img>
+            </div>
+            <div className="list">
+                <ul>
+                </ul>
+            </div>
+            </div>
+        </div>
+
+    );
+
+}
+
+export default About
--- src/Hero.jsx
+++ src/Hero.jsx
@@ -6,10 +6,13 @@ function Hero(){
                 <div className="hero">
                 <div className="mini-heading">
                     <p>Hi there, my name is</p>
+                </div>
                 <div className="name">
                     <h4>JM JUNGCO</h4>
+                </div>
                 <div className="content">
                     <h4>I design and develop for the users.</h4>
+                </div>
                 <div className="mini-intro">
                     <p>I’m a student at Western Institute of Technology partaking the course of Information of Technology.</p>
                     <div className="up-line"></div>
@@ -17,10 +20,7 @@ function Hero(){
                     <p className="line"> scroll</p>
                 </div>
                 <div className="bot-line"></div>
-                </div>
-                </div>
-                </div>
-                </div>
+                </div>                
                 </div>
                 </div>
         </section>
