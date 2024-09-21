c6f4626f6cb33482135aedda4428f434ebe42fa6
john micheal jungco <138555806+jungco15@users.noreply.github.com>
2024-09-21 11:06:19 +0800

    commit

--- src/About.jsx
+++ src/About.jsx
@@ -5,12 +5,21 @@ function About(){
 
         <div className="container">
             <div className="section-header">
-                <h5></h5>
+            <h5>About Me</h5>
             </div>
             <div className="line"></div>
             <div className="content">
-                 <div className="body-left">
-                    <p></p>
+                <div className="body-left">
+                <p>Hi, I'm Jm and I grew up in Antique province. Now, I live in a dorm in San Miguel. My interest in computer started when I was 12 years old and I learned a lot of stuff through the internet. I did my upper secondary education in ICT from Wright Technological College of Antique.</p>
+                <p>Now, I’m a student at Western Institute of Technology. My hobbies these days is building a accessible website through React and play League of legends on my free time. Meanwhile, my main focus is UX design and become a Entry-level UX designer. My career goal is either to be a Motion designer or a Visual designer and I plan to pursue UI design and Game design.</p>
+            <div className="bottom-content">
+                <div className="bottom-header">
+                <p>Here are some technologies I’ve been working on lately:</p>
+                </div>
+            <div className="bottom-list">
+                <ul></ul>
+            </div>
+            </div>
                  </div>
             <div className="body-right">
                     <img></img>
--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import Header from './Header.jsx'
 import './Header.css';
 import Hero from './Hero.jsx'
 import './Hero.css'
+import About from './About.jsx'
+import './About.css'
 
 
 
@@ -13,6 +15,7 @@ function App() {
     <>
     <Header/>
     <Hero/>
+    <About/>
     </>
 
 
